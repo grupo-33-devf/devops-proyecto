f9a991da8ed0f368f40a1f1cec56f5b0483b63f0
servo pc <fernandonando98@live.com.mx>
2025-02-18 23:51:04 -0600

    chore: pre split

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,9 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   server: {
+    watch: {
+      usePolling: true,
+    },
     host: true,
     strictPort: true,
     port: 5173,
